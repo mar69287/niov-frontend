e58569395a733cf26960cd94049cf8f48f47ce37
mar69287 <marcoa.ruiz97@gmail.com>
2024-08-01 22:17:37 -0700

    responsive cards

--- app/federation-cloud/page.js
+++ app/federation-cloud/page.js
@@ -37,22 +37,35 @@ const FederationCloudPage = () => {
 const Cards = ({ subTitle, title, details }) => {
     return (
         <GradientBox
-            className={`bg-gradient-to-t rounded-md p-[1.2px] md:p-[1.4px] w-full overflow-hidden z-5`}
+            className={`bg-gradient-to-tr rounded-md p-[1.2px] md:p-[1.4px] w-full overflow-hidden z-5 md:col-span-2`}
         >
             <div
-                className={`bg-white rounded-md p-5 md:p-7 lg:p-10 flex-start flex-col gap-1 h-[20rem] relative overflow-hidden`}
+                className={`bg-white rounded-md p-5 md:p-7 lg:p-10 flex-start flex-col lg:flex-row lg:gap-5 gap-1 h-[22rem] md:h-[25rem] relative overflow-hidden`}
             >
-                {
-                    subTitle && (
-                        <p className="text-xs md:text-sm lg:text-base m-0 text-gray-500">
-                            {subTitle}
-                        </p>
-                    )
-                }
-                <h1 className="text-xl md:text-2xl lg:text-3xl text-start font-bold leading-6">{title}</h1>
-                <p className="text-xs md:text-sm lg:text-base m-0 max-w-lg">
-                    {details}
-                </p>
+                <div className="flex-start flex-col gap-1 lg:flex-1">
+                    {
+                        subTitle && (
+                            <p className="text-xs md:text-sm lg:text-base m-0 text-gray-500">
+                                {subTitle}
+                            </p>
+                        )
+                    }
+                    <h1 className="text-xl md:text-2xl lg:text-3xl text-start font-bold leading-6">{title}</h1>
+                    <p className="text-xs md:text-sm lg:text-base m-0 lg:max-w-md">
+                        {details}
+                    </p>
+                </div>
+                <div 
+                    className="w-full flex-1 relative mt-3 sm:mt-5"
+                >
+                    <GradientBox className={`bg-gradient-to-tr rounded-md p-[1.1px] absolute top-0 left-0 right-0 lg:w-[45rem] min-[500px]:w-full h-[20rem] lg:h-[25rem]`}>
+                        <div 
+                            className="bg-white rounded-md w-full h-full"
+                        >
+
+                        </div>
+                    </GradientBox>
+                </div>
             </div>
         </GradientBox>
     )
