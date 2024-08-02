10e6b6f86c01964126540820a37c051523ff424a
mar69287 <marcoa.ruiz97@gmail.com>
2024-08-01 21:51:18 -0700

    transfer data and new component

--- app/page.js
+++ app/page.js
@@ -3,51 +3,8 @@ import GradientBox from "./components/GradientBox";
 import ImageContainer from "./components/ImageContainer";
 import EmailButton from "./components/buttons/EmailButton";
 import Hero from "./components/Hero";
-
-const heroCardInfo = [
-  {
-    name: "Federation Cloud",
-    icon: '/assets/images/fedCloudLogo.svg',
-    details: "Access high-quality aggregated data, including users` staked decentralized identities, to optimize marketing efforts and create more effective, trust-building campaigns.",
-  },
-  {
-    name: "NIOV Wallet",
-    icon: '/assets/images/walletIcon.svg',
-    details: "Manage your userbase and send personalized in-wallet messages, marketing campaigns and ads to your users, while empowering them to stake their data for rewards.",
-  },
-  {
-    name: "Decentralized Privacy Sandbox",
-    icon: '/assets/images/sandIcon.svg',
-    details: "Advertise directly with your audiences securely and transparently, eliminating the middleman and ensuring complete user privacy and data control.",
-  },
-]
-
-const productCardInfo = [
-  {
-    name: "User Analytics Dashboard",
-    details: "Track and analyze user engagement with detailed metrics and insights. Optimize your campaigns for maximum impact.",
-  },
-  {
-    name: "Personalized Messaging",
-    details: "Send customized in-wallet messages tailored to your users' preferences and behavior. Increase engagement and conversion rates.",
-  },
-  {
-    name: "Multi-Channel Campaigns",
-    details: "Design and execute marketing campaigns across various blockchain networks, including cross-platform campaigns. Reach your audience wherever they are.",
-  },
-  {
-    name: "Real-Time Reporting",
-    details: "Access real-time data and reports to monitor your campaign performance. Make informed decisions with up-to-date information.",
-  },
-  {
-    name: "Real-Time User Engagement Notifications",
-    details: "Get real-time notifications from Federation Cloud and Privacy Sandbox about user actions in the NIOV Wallet for quick follow-ups.",
-  },
-  {
-    name: "Secure Data Management",
-    details: "Secure user data with encryption and IPFS for storage. Protect privacy, build trust, and ensure compliance.",
-  }
-]
+import { homeCardInfo, homeProductCardInfo, finalCardInfo } from "@/utils/data";
+import FinalCard from "./components/cards/FinalCard";
 
 export default function Home() {
   return (
@@ -79,7 +36,7 @@ export default function Home() {
         <div
           className="grid grid-cols-1 md:grid-cols-2 gap-5 w-full mt-4 md:mt-6 lg:mt-10"
         >
-          {heroCardInfo.map((card, index) => (
+          {homeCardInfo.map((card, index) => (
             <HeroCards 
               key={index}
               name={card.name}
@@ -94,7 +51,7 @@ export default function Home() {
             >
               <h1 className="text-xl md:text-2xl lg:text-3xl max-w-sm lg:max-w-md text-start font-semibold leading-6">Intuitive Products Engineered for the Enterprise.</h1>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 w-full mt-4 md:mt-6 lg:mt-10">
-                {productCardInfo.map((card, index) => (
+                {homeProductCardInfo.map((card, index) => (
                   <ProductCards
                     key={index}
                     name={card.name}
@@ -104,15 +61,7 @@ export default function Home() {
               </div>
             </div>
           </GradientBox>
-          <div className={`rounded-md p-5 md:p-7 lg:p-10 w-full z-5 md:col-span-2 bg-[#94FFC9] h-52 md:h-56 lg:h-60 flex-start flex-col gap-2`}>
-            <h1 className="text-xl md:text-2xl lg:text-3xl text-start font-bold leading-6 max-w-sm lg:max-w-xl">Experience the “New Internet of Value” with NIOV Labs</h1>
-            <p className="text-xs md:text-base lg:text-lg m-0 text-gray-600">
-              Transform you marketing strategy with the power of Web3
-            </p>
-            <EmailButton className="border-none mt-2 w-max px-5 text-black font-light bg-white text-xs md:text-sm lg:text-base h-[2.2rem] md:h-[2.2rem] lg:h-[2.5rem] min-h-[1.5rem] max-h-[2.5rem]">
-              Request a demo
-            </EmailButton>
-          </div>
+          <FinalCard title={finalCardInfo.title} details={finalCardInfo.details} />
         </div>
       </div>
     </section>
